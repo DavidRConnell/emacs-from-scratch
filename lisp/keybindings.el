d3591d432a0671a341583f1cc165d6109563b470
David R Connell <davidconnell12@gmail.com>
2020-09-20 17:35:26 -0500

    Add: setup basic evil bindings

--- lisp/keybindings.el
+++ lisp/keybindings.el
@@ -1,7 +1,19 @@
 ;;   general   for binding keys also look into use-package's method might not be needed.
 ;;   evil
 
+(use-package general
+  :config
+  (general-evil-setup)
+  (defconst dc-leader "SPC")
+  (defconst dc-local-leader ","))
+
 (use-package evil
+  :general
+  (general-nmap "J" #'evil-scroll-line-down
+                "K" #'evil-scroll-line-up
+                "H" #'evil-beginning-of-visual-line
+                "L" #'evil-end-of-line-or-visual-line
+                "gj" #'evil-join)
   :config
   (evil-mode 1))
 
