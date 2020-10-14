6263d358e1cad33f0bf7415d609e6a1e249bd2ab
David R Connell <davidconnell12@gmail.com>
2020-10-14 18:21:37 -0500

    Change: try out selectrum and friends
    - In place of ivy and friends.

--- lisp/completion.el
+++ lisp/completion.el
@@ -3,7 +3,27 @@
 ;;   yas
 ;;   file-snippets or whatever it is. Let's make sure keeping good formed files.
 
+(use-package prescient
+  :config
+  (prescient-persist-mode +1))
+
+(use-package selectrum
+  :config
+  (use-package selectrum-prescient)
+  (selectrum-mode +1)
+  (selectrum-prescient-mode +1))
+
+(use-package mini-frame
+  :config
+  (mini-frame-mode +1)
+  (setq mini-frame-show-parameters '((top . 0.25) (width . 0.7) (left . 0.5))))
+
+(use-package ctrlf
+  :config
+  (ctrlf-mode +1))
+
 (use-package ivy
+  :disabled
   :config
   (use-package ivy-posframe
     :hook (ivy-mode . ivy-posframe-mode))
