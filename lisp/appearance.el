57fc520e0b64c1947c45127a58e967d29f401f6a
David R Connell <davidconnell12@gmail.com>
2020-10-25 16:59:45 -0500

    Add: toggle themes keybinding
    - Add a function for toggling between light and dark themes instead of
    having to select one manually. There are only two options so it is
    pointless to select them.
    - Also add to a keybinding for quick changes.
    
      #
           (use "git push" to publish your local commits)

--- lisp/appearance.el
+++ lisp/appearance.el
@@ -1,21 +1,32 @@
 (use-package modus-operandi-theme)
 (use-package modus-vivendi-theme)
 
-(defvar my-light-theme 'modus-operandi
-  "Default light theme.")
-(defvar my-dark-theme 'modus-vivendi
-  "Default dark theme.")
+(defvar my-light-theme 'modus-operandi "Default light theme.")
+(defvar my-dark-theme 'modus-vivendi "Default dark theme.")
+(defvar my-current-theme-light-p nil)
 
 (defun my-set-theme (&optional type)
   "Load a theme. Select a TYPE, light or dark."
   (interactive)
   (if (not type)
       (setq type (completing-read "Type: " '(light dark))))
-  (if (eq type 'light)
-      (load-theme my-light-theme t)
-    (load-theme my-dark-theme t)))
+  (if (string-equal (format "%s" type) "light")
+      (progn
+	(load-theme my-light-theme t)
+	(setq my-current-theme-light-p t))
+    (progn
+      (load-theme my-dark-theme t)
+      (setq my-current-theme-light-p nil))))
+
+(defun my-toggle-theme ()
+  "Toggle between my light and dark themes."
+  (interactive)
+  (if my-current-theme-light-p
+      (my-set-theme 'dark)
+    (my-set-theme 'light)))
 
 (my-set-theme 'light)
+(general-nmap :prefix "C-h" "t" #'my-toggle-theme)
 
 (use-package delight)
 
