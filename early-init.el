284ae02f924800e7292cae4a33f9507a6cfd2092
David R Connell <davidconnell12@gmail.com>
2020-10-25 16:53:40 -0500

    Fix: header formatting

--- early-init.el
+++ early-init.el
@@ -1,6 +1,15 @@
 ;;; early-init.el -*- lexical-binding: t; -*-
 ;;
-;; Copyright (C) 2020 David R. Connell ;; ;; Author: David R. Connell <http://github/voidee> ;; Maintainer: David R. Connell <voidee@TheVoid> ;; Created: October 13, 2020 ;; Modified: October 13, 2020 ;; Version: 0.0.1 ;; Keywords: ;; Homepage: https://github.com/voidee/early-init ;; Package-Requires: ((emacs 27.1) (cl-lib "0.5"))
+;; Copyright (C) 2020 David R. Connell
+;;
+;; Author: David R. Connell <http://github/voidee>
+;; Maintainer: David R. Connell <voidee@TheVoid>
+;; Created: October 13, 2020
+;; Modified: October 13, 2020
+;; Version: 0.0.1
+;; Keywords:
+;; Homepage: https://github.com/voidee/early-init
+;; Package-Requires: ((emacs 27.1) (cl-lib "0.5"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
