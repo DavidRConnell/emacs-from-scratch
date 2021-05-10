b66a166262625b51fd246d4af1dd8dee28f99da4
David R Connell <davidconnell12@gmail.com>
2021-05-10 16:23:14 -0500

    Add: todo and other keyword highlighting

--- lisp/development.el
+++ lisp/development.el
@@ -246,6 +246,18 @@ This ensures the results are visible."
 (use-package prism
   :straight (prism :host github :repo "alphapapa/prism.el")
   :commands (prism-whitespace-mode prism-mode)
+(use-package hl-todo
+    :hook (prog-mode . hl-todo-mode)
+    :config
+    (setq hl-todo-highlight-punctuation ":"
+          hl-todo-keyword-faces
+          `(("TODO"       warning bold)
+            ("FIXME"      error bold)
+            ("HACK"       font-lock-constant-face bold)
+            ("REVIEW"     font-lock-keyword-face bold)
+            ("NOTE"       success bold)
+            ("DEPRECATED" font-lock-doc-face bold))))
+
   :general
   (my-leader-def
     :infix "h"
