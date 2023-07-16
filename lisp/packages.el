6c3d2e03d54ca3f7382351ddf354df59f001da0c
David R Connell <davidconnell12@gmail.com>
2023-07-16 12:19:18 -0500

    Update straight

--- lisp/packages.el
+++ lisp/packages.el
@@ -3,13 +3,14 @@
 (defvar straight-base-dir my-cache-dir)
 (defvar bootstrap-version)
 (defvar straight-check-for-modifications '(check-on-save find-when-checking))
+(setq straight-repository-branch "develop")
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" straight-base-dir))
-      (bootstrap-version 5))
+      (bootstrap-version 6))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
         (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
          'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
