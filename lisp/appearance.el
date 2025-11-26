07def32bd56d46480cb8333eaf51dbb1da420859
David R Connell <david32@dcon.addy.io>
2025-11-25 22:06:30 -0600

    Add eww for basic html reading

--- lisp/appearance.el
+++ lisp/appearance.el
@@ -20,4 +20,40 @@
 		    :underline 'unspecified :overline 'unspecified
 		    :box 'unspecified :inherit 'bold)
 
+(use-package eww
+  :config
+  (defun mozilla-readable (url)
+    (let ((buff (format "*readable-%s*" url)))
+      (with-current-buffer (get-buffer-create buff)
+	(pop-to-buffer (current-buffer))
+	(shell-command (format "readable %s" url) (current-buffer))
+	(eww-display-html 'utf-8 (buffer-name) nil (point-min) (current-buffer)))))
+
+  (defun eww-readable ()
+    "View the main \"readable\" parts of the current web page.
+This command uses heuristics to find the parts of the web page that
+contains the main textual portion, leaving out navigation menus and
+the like."
+    (interactive nil eww-mode)
+    (let* ((old-data eww-data)
+	   (dom (with-temp-buffer
+		  (insert (plist-get old-data :source))
+		  (condition-case nil
+		      (decode-coding-region (point-min) (point-max) 'utf-8)
+		    (coding-system-error nil))
+		  (shell-command-on-region (point-min) (point-max)
+					   "readable" nil t)
+                  (eww--preprocess-html (point-min) (point-max))
+		  (libxml-parse-html-region (point-min) (point-max))))
+           (base (plist-get eww-data :url)))
+      (eww-score-readability dom)
+      (eww-save-history)
+      (eww-display-html nil nil
+			(list 'base (list (cons 'href base))
+                              (eww-highest-readability dom))
+			nil (current-buffer))
+      (dolist (elem '(:source :url :title :next :previous :up :peer))
+	(plist-put eww-data elem (plist-get old-data elem)))
+      (eww--after-page-change))))
+
 (provide 'appearance)
