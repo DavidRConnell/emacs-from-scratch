fb70f3aa7c411c298f02c3766ddccdad0c005437
David R Connell <david32@dcon.addy.io>
2025-11-25 22:05:56 -0600

    Update theme colors
    - And remove modus-operandi now that it is shipped with emacs

--- lisp/appearance.el
+++ lisp/appearance.el
@@ -2,11 +2,10 @@
 ;;; Commentary:
 ;;; Code:
 
-(use-package modus-operandi-theme)
-
 (load-theme 'modus-operandi t)
 (setq-default mode-line-format nil)
 (blink-cursor-mode -1)
+(setq-default fill-column 79)
 (global-display-fill-column-indicator-mode t)
 (show-paren-mode t)
 
@@ -14,5 +13,11 @@
   :hook (visual-line-mode . visual-fill-column-mode))
 
 (require 'mode-line)
+(set-face-attribute 'modus-themes-completion-selected nil
+		    :foreground 'unspecified :background nano-color-highlight
+		    :family 'unspecified :slant 'unspecified
+		    :weight 'unspecified :height 'unspecified
+		    :underline 'unspecified :overline 'unspecified
+		    :box 'unspecified :inherit 'bold)
 
 (provide 'appearance)
