0ba9f76eb79128a7006c83b4fcfcbcb1ab17c52e
David R Connell <david32@dcon.addy.io>
2025-11-25 21:59:54 -0600

    Update templates

--- file-templates/00:.*.el
+++ file-templates/00:.*.el
@@ -1,15 +1,30 @@
-;;; `(file-name-nondirectory buffer-file-name)`${1: --- ${2:Description}} -*- lexical-binding: t; -*-
-;;
+;;; `(file-name-base buffer-file-name)` --- $1 -*- lexical-binding: t; -*-
+
 ;; Copyright (C) `(format-time-string "%Y")` `user-full-name`
 ;;
-;; Maintainer: `user-full-name` <`user-mail-address`>
-;; Created: `(format-time-string "%B %d, %Y")`
-;; Modified: `(format-time-string "%B %d, %Y")`
-;; Version: 0.0.1
-;;
+;; Author: `user-full-name` <`user-mail-address`>
+;; Version: 0.1.0
+
 ;; This file is not part of GNU Emacs.
-;;
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or (at
+;; your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
 ;;; Commentary:
+;; $2
+
 ;;; Code:
 
 `%`$0
---
+++ file-templates/01:.*.c
@@ -0,0 +1,19 @@
+/* Copyright `(format-time-string "%Y")` `user-full-name` <`user-mail-address`>.
+ *
+ * This file is part of $1.
+ *
+ * $1 is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * $1 is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with $1. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+$0
