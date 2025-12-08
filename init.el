f504d2f36b3134083c471d94519c81dfc55f7018
David R Connell <david32@dcon.addy.io>
2025-12-08 16:55:55 -0600

    Remove unused modules for now
    - And update header.

--- init.el
+++ init.el
@@ -1,5 +1,30 @@
-;;;; init.el --- Load modules -*- lexical-binding: t; -*-
+;;;; init.el --- Main initialization file -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020 David R. Connell
+;;
+;; Author: David R. Connell <david32@dcon.addy.io>
+;; Created: October 13, 2020
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or (at
+;; your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
 ;;; Commentary:
+;; Dispatcher for requiring all Emacs initialization files.
+
 ;;; Code:
 
 (add-to-list 'load-path
@@ -19,10 +44,8 @@
 (require 'notes)
 (require 'references)
 (require 'file-templates)
-(require 'mail)
 (require 'terminal)
 (require 'rss)
-(require 'chat)
 (require 'completion)
 (require 'speed)
 
